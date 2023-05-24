223a70039d5f591af2484724dd9a2c1188c0dfe2
salikbubu <salikbula@gmail.com>
2023-05-24 10:38:40 +0200

    components

---
+++ client/src/components/Slider.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+interface SliderProps {
+    slides: {
+        url: string;
+        label: string;
+        subtitle: string;
+    }[];
+}
+
+const Slider: React.FC<SliderProps> = ({slides}) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Slider
